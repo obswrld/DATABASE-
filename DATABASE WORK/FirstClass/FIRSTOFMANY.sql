4b63795254cdd2298b9ca7880b84f9a89bb903e5
Republic Oba <republicoba1@gmail.com>
2025-08-13 07:09:48 +0100

    new works from the database

--- DATABASE WORK/FirstClass/FIRSTOFMANY.sql
+++ DATABASE WORK/FirstClass/FIRSTOFMANY.sql
@@ -1,40 +1,64 @@
 -- 2.40 --
-SELECT SKU, SKU_Description, WAREHOUSE.WarehouseID, INVENTORY.WarehouseID, WarehouseCity, WarehouseState
-FROM WAREHOUSE, INVENTORY
-WHERE WAREHOUSE.WarehouseID = INVENTORY.WarehouseID
-AND (WarehouseCity = "ATLANTA" OR WarehouseCity = "Bango" OR WarehouseCity = "Chicargo warehouse");
+-- SELECT SKU, SKU_Description, WAREHOUSE.WarehouseID, INVENTORY.WarehouseID, WarehouseCity, WarehouseState
+-- FROM WAREHOUSE, INVENTORY
+-- WHERE WAREHOUSE.WarehouseID = INVENTORY.WarehouseID
+-- AND (WarehouseCity = "ATLANTA" OR WarehouseCity = "Bango" OR WarehouseCity = "Chicargo warehouse");
 
 -- 2.41 --
-SELECT SKU, SKU_Description, WAREHOUSE.WarehouseID, INVENTORY.WarehouseID, WarehouseCity, WarehouseState
-FROM WAREHOUSE, INVENTORY
-WHERE WarehouseCity IN ("ATLANTA", "BANGO", "CHICARGO WAREHOUSE");
+-- SELECT SKU, SKU_Description, WAREHOUSE.WarehouseID, INVENTORY.WarehouseID, WarehouseCity, WarehouseState
+-- FROM WAREHOUSE, INVENTORY
+-- WHERE WarehouseCity IN ("ATLANTA", "BANGO", "CHICARGO WAREHOUSE");
 
 -- 2.42 --
-SELECT SKU, SKU_Description, WAREHOUSE.WarehouseID, INVENTORY.WarehouseID, WarehouseCity, WarehouseState
-FROM WAREHOUSE, INVENTORY
-WHERE WAREHOUSE.WarehouseID = INVENTORY.WarehouseID
-AND NOT (WarehouseCity = "ATLANTA" OR WarehouseCity = "Bango" OR WarehouseCity = "Chicargo warehouse");
+-- SELECT SKU, SKU_Description, WAREHOUSE.WarehouseID, INVENTORY.WarehouseID, WarehouseCity, WarehouseState
+-- FROM WAREHOUSE, INVENTORY
+-- WHERE WAREHOUSE.WarehouseID = INVENTORY.WarehouseID
+-- AND NOT (WarehouseCity = "ATLANTA" OR WarehouseCity = "Bango" OR WarehouseCity = "Chicargo warehouse");
 
 -- 2.43 --
-SELECT SKU, SKU_Description, WAREHOUSE.WarehouseID, INVENTORY.WarehouseID, WarehouseCity, WarehouseState
-FROM WAREHOUSE, INVENTORY
-WHERE WarehouseCity NOT IN ("ATLANTA", "BANGO", "CHICARGO WAREHOUSE");
+-- SELECT SKU, SKU_Description, WAREHOUSE.WarehouseID, INVENTORY.WarehouseID, WarehouseCity, WarehouseState
+-- FROM WAREHOUSE, INVENTORY
+-- WHERE WarehouseCity NOT IN ("ATLANTA", "BANGO", "CHICARGO WAREHOUSE");
 
 -- 2.44 --
-SELECT CONCAT(SKU_Description , "is Located in" , WarehouseCity)
-AS ItemLocation
-FROM WAREHOUSE, INVENTORY;
+-- SELECT CONCAT(SKU_Description , "is Located in" , WarehouseCity)
+-- AS ItemLocation
+-- FROM WAREHOUSE, INVENTORY;
 
 -- 2.45 -- 
-SELECT SKU, SKU_Description, WarehouseID
+-- SELECT SKU, SKU_Description, WarehouseID
+-- FROM INVENTORY
+-- WHERE WarehouseID IN (
+-- 	SELECT WarehouseID
+--     
+-- 	FROM Warehouse
+--     
+--     WHERE Manager = "Lucille Smith"
+-- );
+
+-- 2.46 --
+-- SELECT SKU, SKU_Description, INVENTORY.WarehouseID
+-- FROM INVENTORY, WAREHOUSE
+-- WHERE Manager = "Lucille Smith"
+-- AND INVENTORY.WarehouseID = WAREHOUSE.WarehouseID;
+
+-- 2.47 --
+-- SELECT SKU, SKU_Description, INVENTORY.WarehouseID
+-- FROM INVENTORY
+-- JOIN WAREHOUSE ON INVENTORY.WarehouseID = WAREHOUSE.WarehouseID
+-- WHERE Manager = "Lucille Smith";
+
+-- 2.48 --
+SELECT WarehouseID, AVG(QuantityOnHand) AS AverageQunatityOnHand
 FROM INVENTORY
 WHERE WarehouseID IN (
-	SELECT WarehouseID
+	SELECT warehouseID
     
-	FROM Warehouse
+    FROM WAREHOUSE
     
     WHERE Manager = "Lucille Smith"
-);
+)
 
+GROUP BY WarehouseID;
 
 
